20472179c45e1f14d4a3128c801538ac9ea3f0bb
Oleksii Pavlenko <oleksa.pavlenko@gmail.com>
2021-04-15 08:33:48 +0000

    Attempt to fix deprecation messages.

--- core/context.py
+++ core/context.py
@@ -11,36 +11,33 @@ class Context(commands.Context):
 
     async def post_info(self, text: str):
         """Send info message."""
-        return await self.send(
-            embed=discord.Embed(
-                description=text,
-                title=":information_source:",
-                color=discord.Color.blue(),
-                footer=DISCONTINUED,
-            )
+        embed = discord.Embed(
+            description=text,
+            title=":information_source:",
+            color=discord.Color.blue(),
         )
+        embed.set_footer(text=DISCONTINUED)
+        return await self.send(embed)
 
     async def post_warning(self, text: str):
         """Send warning message."""
-        return await self.send(
-            embed=discord.Embed(
-                description=text,
-                title=":warning:",
-                color=discord.Color.orange(),
-                footer=DISCONTINUED,
-            )
+        embed=discord.Embed(
+            description=text,
+            title=":warning:",
+            color=discord.Color.orange(),
         )
+        embed.set_footer(text=DISCONTINUED)
+        return await self.send(embed)
 
     async def post_error(self, text: str):
         """Send error message."""
-        return await self.send(
-            embed=discord.Embed(
-                description=text,
-                title=":no_entry:",
-                color=discord.Color.red(),
-                footer=DISCONTINUED,
-            )
+        embed=discord.Embed(
+            description=text,
+            title=":no_entry:",
+            color=discord.Color.red(),
         )
+        embed.set_footer(text=DISCONTINUED)
+        return await self.send(embed)
 
     async def react_ok(self) -> discord.Reaction:
         """Add ok hand reaction to the message."""
